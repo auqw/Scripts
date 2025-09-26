521b4759d4d275232a7c23f1881cf633289ab8a1
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-26 12:30:35 -0300

    Update UltraEngineer.cs

--- Ultras/UltraEngineer.cs
+++ Ultras/UltraEngineer.cs
@@ -1,4 +1,4 @@
-//cs_include Scripts/WIP/CoreUltras.cs
+//cs_include Scripts/Ultras/CoreUltras.cs
 
 using System;
 using System.Dynamic;
@@ -30,3 +30,4 @@ public class UltraEngineer
             Core.KillWithPriority("Ultra Engineer", 3, "Defense Drone", 2, "Attack Drone", 1);
     }
 }
+
