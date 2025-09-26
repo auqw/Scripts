519fa345d0067c6df2cfcdc4b509d9eb5248a424
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-26 12:29:34 -0300

    Update UltraAvatarTyndarius.cs

--- Ultras/UltraAvatarTyndarius.cs
+++ Ultras/UltraAvatarTyndarius.cs
@@ -1,4 +1,4 @@
-//cs_include Scripts/WIP/CoreUltras.cs
+//cs_include Scripts/Ultras/CoreUltras.cs
 
 using System;
 using System.Dynamic;
@@ -40,3 +40,4 @@ public class UltraAvatarTyndarius
         }
     }
 }
+
