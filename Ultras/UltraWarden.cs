9d15db198189b81a7cb56cfff31f1910ad394158
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-26 12:30:17 -0300

    Update UltraWarden.cs

--- Ultras/UltraWarden.cs
+++ Ultras/UltraWarden.cs
@@ -1,4 +1,4 @@
-//cs_include Scripts/WIP/CoreUltras.cs
+//cs_include Scripts/Ultras/CoreUltras.cs
 
 using System;
 using System.Dynamic;
@@ -36,3 +36,4 @@ public class UltraWarden
                 Core.Attack("Ultra Warden");
     }
 }
+
