c4d472b4eae9b58b42a0bf16bbdf1c7a957a24b5
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-27 16:05:47 -0300

    Update UltraDrago.cs

--- Ultras/UltraDrago.cs
+++ Ultras/UltraDrago.cs
@@ -10,28 +10,57 @@ public class UltraDrago
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreUltras Core = new();
 
+    public string primaryTaunter;
+    public string secondaryTaunter;
     public bool DontPreconfigure = true;
     public string OptionsStorage = "UltraDrago";
     public List<IOption> Options = new()
     {
-        new Option<string>("primaryTaunter", "First Taunter Class", "Insert the name of the class that will taunt", "ArchPaladin"),
-        new Option<string>("secondaryTaunter", "Second Taunter Class", "Insert the name of the class that will taunt", "Lord Of Order"),
+        new Option<string>("primaryTaunter", "First Taunter Class", "Insert the name of the class that will taunt", ""),
+        new Option<string>("secondaryTaunter", "Second Taunter Class", "Insert the name of the class that will taunt", ""),
     };
 
     public void ScriptMain(IScriptInterface bot)
     {
+        primaryTaunter = Bot.Config.Get<string>("primaryTaunter") ?? string.Empty;
+        if (string.IsNullOrEmpty(primaryTaunter))
+        {
+            Bot.Log("First taunter not filled in! Please edit Script Options.");
+            Bot.Stop();
+        }
+
+        secondaryTaunter = Bot.Config.Get<string>("secondaryTaunter") ?? string.Empty;
+        if (string.IsNullOrEmpty(secondaryTaunter))
+        {
+            Bot.Log("Second taunter not filled in! Please edit Script Options.");
+            Bot.Stop();
+        }
+
         Core.Boot();
 
-        Kill(primaryTaunter: Bot.Config.Get<string>("primaryTaunter"), secondaryTaunter: Bot.Config.Get<string>("secondaryTaunter"));
+        Bot.Quests.UpdateQuest(8395);
+        PreparePotions(primaryTaunter, secondaryTaunter);
+        Kill(primaryTaunter, secondaryTaunter);
 
         Bot.Stop();
     }
 
-    void Kill(string primaryTaunter, string secondaryTaunter)
+    void PreparePotions(string primaryTaunter, string secondaryTaunter)
     {
+        Core.UseAlchemyPotions(Core.GetBestTonicPotion());
+        Core.UseAlchemyPotions(Core.GetBestElixirPotion());
+
         if (Core.HasClassEquipped(primaryTaunter) || Core.HasClassEquipped(secondaryTaunter))
             Core.GetScrollOfEnrage();
+        else
+        {
+            Core.BuyAlchemyPotion("Potent Honor Potion");
+            Core.EquipConsumable("Potent Honor Potion");
+        }
+    }
 
+    void Kill(string primaryTaunter, string secondaryTaunter)
+    {
         Core.Join("ultradrago");
         Core.WaitForArmy(3);
         Core.ChooseBestCell("King Drago");
@@ -49,9 +78,9 @@ public class UltraDrago
                 }
             }
             else
+            {
                 Core.KillWithPriority("King Drago", "Bowmaster Algie", "Executioner Dene");
+                Bot.Skills.UseSkill(5);
+            }
     }
 }
-
-
-
