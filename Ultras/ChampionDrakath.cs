e4d2f3d0358c86fd5140afa28f5f1ca876fe31e4
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-27 16:14:57 -0300

    Update ChampionDrakath.cs

--- Ultras/ChampionDrakath.cs
+++ Ultras/ChampionDrakath.cs
@@ -10,24 +10,44 @@ public class ChampionDrakath
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreUltras Core = new();
 
+    public string taunterClass;
     public bool DontPreconfigure = true;
     public string OptionsStorage = "ChampionDrakath";
-    public List<IOption> Options = new() { new Option<string>("taunterClass", "Taunter Class", "Insert the name of the class that will taunt", "ArchPaladin"), };
+    public List<IOption> Options = new() {
+        new Option<string>("taunterClass", "Taunter Class", "Insert the name of the class that will taunt", "ArchPaladin")
+    };
 
     public void ScriptMain(IScriptInterface bot)
     {
+        taunterClass = Bot.Config.Get<string>("taunterClass") ?? string.Empty;
+        if (string.IsNullOrEmpty(taunterClass))
+        {
+            Bot.Log("Taunter not filled in! Please edit Script Options.");
+            Bot.Stop();
+        }
+
         Core.Boot();
 
-        Kill(taunterClass: Bot.Config.Get<string>("taunterClass"));
+        PreparePotions(taunterClass);
+        Kill(taunterClass);
 
         Bot.Stop();
     }
 
-    void Kill(string taunterClass)
+    void PreparePotions(string taunterClass)
     {
+        Core.UseAlchemyPotions(Core.GetBestTonicPotion());
+        Core.UseAlchemyPotions(Core.GetBestElixirPotion());
+
+        Core.BuyAlchemyPotion("Potent Honor Potion");
+        Core.EquipConsumable("Potent Honor Potion");
+
         if (Core.HasClassEquipped(taunterClass))
             Core.GetScrollOfEnrage();
+    }
 
+    void Kill(string taunterClass)
+    {
         Core.Join("championdrakath");
         Core.WaitForArmy(3);
         Core.ChooseBestCell("Champion Drakath");
@@ -38,7 +58,11 @@ public class ChampionDrakath
             if (Core.HasClassEquipped(taunterClass))
                 Core.DrakathTaunter();
             else
+            {
                 Core.Kill("Champion Drakath");
+                if (Core.GetTargetHealthPercentage() < 10)
+                    Bot.Skills.UseSkill(5);
+            }
         }
     }
 }
