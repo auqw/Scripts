e695eb1bb7baf60fdc1a1779fbbdd0d68e0b243b
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-27 16:18:57 -0300

    Update Xyfrag.cs

--- Ultras/Xyfrag.cs
+++ Ultras/Xyfrag.cs
@@ -10,25 +10,41 @@ public class Xyfrag
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreUltras Core = new();
 
+    public string taunterClass;
     public bool DontPreconfigure = true;
     public string OptionsStorage = "Xyfrag";
-    public List<IOption> Options = new() { new Option<string>("taunterClass", "Taunter Class", "Insert the name of the class that will taunt", "Legion Revenant"), };
+    public List<IOption> Options = new() { new Option<string>("taunterClass", "Taunter Class", "Insert the name of the class that will taunt", ""), };
 
     public void ScriptMain(IScriptInterface bot)
     {
+        taunterClass = Bot.Config.Get<string>("taunterClass") ?? string.Empty;
+        if (string.IsNullOrEmpty(taunterClass))
+        {
+            Bot.Log("Taunter not filled in! Please edit Script Options.");
+            Bot.Stop();
+        }
+
         Core.Boot();
 
-        Kill(taunterClass: Bot.Config.Get<string>("taunterClass"));
+        Kill(taunterClass);
 
         Bot.Stop();
     }
 
     void Kill(string taunterClass)
     {
+        Core.UseAlchemyPotions(Core.GetBestTonicPotion());
+        Core.UseAlchemyPotions(Core.GetBestElixirPotion());
+
         if (Core.HasClassEquipped(taunterClass))
         {
-            Core.GetScrollOfEnrage();
             Bot.Events.ExtensionPacketReceived += Core.ChargeListener;
+            Core.GetScrollOfEnrage();
+        }
+        else
+        {
+            Core.BuyAlchemyPotion("Potent Honor Potion");
+            Core.EquipConsumable("Potent Honor Potion");
         }
 
         Core.Join("voidxyfrag");
@@ -41,8 +57,10 @@ public class Xyfrag
             if (Core.HasClassEquipped(taunterClass))
                 Core.TauntCharge(taunterClass, "Xyfrag", "Focus", 250);
             else
+            {
                 Core.Kill("Xyfrag");
+                Bot.Skills.UseSkill(5);
+            }
         }
     }
 }
-
