c06eb0288460f1b3bd237fd5c69a8a375e2f6f33
vanilla99 <105498073+vanilla99-git@users.noreply.github.com>
2025-09-27 06:10:25 -0300

    Update ChampionDrakath.cs

--- Ultras/ChampionDrakath.cs
+++ Ultras/ChampionDrakath.cs
@@ -1,20 +1,17 @@
-//cs_include Scripts/Ultras/CoreUltras.cs
-
-using System;
-using System.Dynamic;
-using Skua.Core.Interfaces;
-using Skua.Core.Options;
-
 public class ChampionDrakath
 {
     public IScriptInterface Bot => IScriptInterface.Instance;
     public CoreUltras Core = new();
 
+    public bool DontPreconfigure = true;
+    public string OptionsStorage = "ChampionDrakath";
+    public List<IOption> Options = new() { new Option<string>("taunterClass", "Taunter Class", "Insert the name of the class that will taunt", "ArchPaladin"), };
+
     public void ScriptMain(IScriptInterface bot)
     {
         Core.Boot();
 
-        Kill(taunterClass: "ArchPaladin");
+        Kill(taunterClass: Bot.Config.Get<string>("taunterClass"));
 
         Bot.Stop();
     }
@@ -38,6 +35,3 @@ public class ChampionDrakath
         }
     }
 }
-
-
-
