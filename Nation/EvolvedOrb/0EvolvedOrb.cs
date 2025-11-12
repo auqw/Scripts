b5f303beeca5b4bdebf1fdf5dd11da5c0234c161
Tato <47118168+wtffidy@users.noreply.github.com>
2025-11-12 04:06:59 -0500

    Update 0EvolvedOrb.cs

--- Nation/EvolvedOrb/0EvolvedOrb.cs
+++ Nation/EvolvedOrb/0EvolvedOrb.cs
@@ -21,11 +21,16 @@ using Skua.Core.Interfaces;
 public class EvolvedOrb
 {
     public CoreBots Core => CoreBots.Instance;
-    private static CoreNation Nation { get => _Nation ??= new CoreNation(); set => _Nation = value; }    private static CoreNation _Nation;
-    private static EvolvedBloodOrb EBO { get => _EBO ??= new EvolvedBloodOrb(); set => _EBO = value; }    private static EvolvedBloodOrb _EBO;
-    private static EvolvedHexOrb EHO { get => _EHO ??= new EvolvedHexOrb(); set => _EHO = value; }    private static EvolvedHexOrb _EHO;
-    private static EvolvedShadowOrb ESO { get => _ESO ??= new EvolvedShadowOrb(); set => _ESO = value; }    private static EvolvedShadowOrb _ESO;
-    private static EvolvedShadowOrbItems ESOItems { get => _ESOItems ??= new EvolvedShadowOrbItems(); set => _ESOItems = value; }    private static EvolvedShadowOrbItems _ESOItems;
+    private static CoreNation Nation { get => _Nation ??= new CoreNation(); set => _Nation = value; }
+    private static CoreNation _Nation;
+    private static EvolvedBloodOrb EBO { get => _EBO ??= new EvolvedBloodOrb(); set => _EBO = value; }
+    private static EvolvedBloodOrb _EBO;
+    private static EvolvedHexOrb EHO { get => _EHO ??= new EvolvedHexOrb(); set => _EHO = value; }
+    private static EvolvedHexOrb _EHO;
+    private static EvolvedShadowOrb ESO { get => _ESO ??= new EvolvedShadowOrb(); set => _ESO = value; }
+    private static EvolvedShadowOrb _ESO;
+    private static EvolvedShadowOrbItems ESOItems { get => _ESOItems ??= new EvolvedShadowOrbItems(); set => _ESOItems = value; }
+    private static EvolvedShadowOrbItems _ESOItems;
 
     public void ScriptMain(IScriptInterface bot)
     {
