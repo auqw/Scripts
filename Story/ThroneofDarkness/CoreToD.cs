f7a8d810a8c3b27470c2bad224ba9e183605447d
Tato <47118168+wtffidy@users.noreply.github.com>
2025-11-19 14:01:12 -0500

    Update CoreToD.cs

--- Story/ThroneofDarkness/CoreToD.cs
+++ Story/ThroneofDarkness/CoreToD.cs
@@ -809,7 +809,7 @@ public class CoreToD
         if (!Story.QuestProgression(5165))
         {
             Core.EnsureAccept(5165);
-            Farm.DeathPitToken("Death Pit Victory (Captain Defeat)", quant: 1, true);
+            Farm.DeathPitToken("Death Pit Victory (Captain Defeat)", isTemp: true);
             Core.EnsureComplete(5165);
         }
     }
