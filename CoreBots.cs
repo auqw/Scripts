d7bc72b4a8e6668c94582e81a38d6bc01206d393
Tato <47118168+wtffidy@users.noreply.github.com>
2025-11-21 12:37:56 -0500

    Update CoreBots.cs

--- CoreBots.cs
+++ CoreBots.cs
@@ -7058,7 +7058,7 @@ public class CoreBots
                 "OK"
             ).Text == "OK"
         )
-            Process.Start("explorer", "https://github.com/auqw/Skua/releases/tag/latest");
+            Process.Start("explorer", "https://github.com/auqw/Skua/releases/latest");
         Logger(
             $"This script requires Skua {targetVersion} or above. Stopping the script",
             messageBox: true,
