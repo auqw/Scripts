ed6a83d1950f50a5b1ea8fdfbe3f0bc7dffb8b0f
Tato <47118168+wtffidy@users.noreply.github.com>
2025-11-21 13:06:28 -0500

    till regex gets fixed

--- CoreBots.cs
+++ CoreBots.cs
@@ -7045,7 +7045,7 @@ public class CoreBots
     /// Checks, and prompts for the latest Skua Version
     /// <param name="targetVersion">Current Skua Version to Check against</param>
     /// </summary>
-    private void SkuaVersionChecker(string targetVersion = "1.3.0.1")
+    private void SkuaVersionChecker(string targetVersion = "1.3.0.0")
     {
         if (Bot.Version == null || Version.Parse(targetVersion).CompareTo(Bot.Version) <= 0)
             return;
