e62ccedb17418c146a5313409eb870135031de03
Tato <47118168+wtffidy@users.noreply.github.com>
2025-11-28 20:40:58 -0500

    Update BloodTitan[Mem].cs

--- Other/Classes/Members-CLasses/BloodTitan[Mem].cs
+++ Other/Classes/Members-CLasses/BloodTitan[Mem].cs
@@ -7,6 +7,8 @@ tags: blood titan, blood titan tokens, member, class
 //cs_include Scripts/CoreFarms.cs
 //cs_include Scripts/CoreAdvanced.cs
 //cs_include Scripts/Other/MergeShops/BloodTitanMerge[Mem].cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/Story/TitanAttack.cs
 using Skua.Core.Interfaces;
 
 public class BloodTitan
@@ -25,6 +27,12 @@ public class BloodTitan
         set => _BTM = value;
     }
     private static BloodTitanMerge _BTM;
+    private static TitanAttackStory Tas
+    {
+        get => _Tas ??= new TitanAttackStory();
+        set => _Tas = value;
+    }
+    private static TitanAttackStory _Tas;
 
     public void ScriptMain(IScriptInterface Bot)
     {
@@ -40,6 +48,7 @@ public class BloodTitan
         if (Core.CheckInventory(16641, toInv: false))
             return;
 
+        Tas.DoAll();
         BTM.BuyAllMerge("Blood Titan");
 
         if (rankUpClass)
