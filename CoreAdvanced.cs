2dfea37bf3ce32297420ac00febddf92760581aa
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-01 08:36:31 -0500

    Update CoreAdvanced.cs

--- CoreAdvanced.cs
+++ CoreAdvanced.cs
@@ -3291,7 +3291,8 @@ public class CoreAdvanced
                 #region Unassigned Region
 
                 // This list serves as an overview of what classes dont have a Forge Enhancement yet, when adding a setup for it, remove it from here
-                case "Phantom Chronomancer":
+                case "phantom chronomancer":
+                case "phantasm chronomancer":
                 case "acolyte":
                 case "alpha doommega":
                 case "alpha omega":
