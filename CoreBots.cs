c3aee12c4a02b7539d6a01044f371cda343469c7
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-08 09:19:40 -0500

    Update CoreBots.cs

--- CoreBots.cs
+++ CoreBots.cs
@@ -3709,8 +3709,11 @@ public class CoreBots
     /// </summary>
     /// <param name="QuestID">The ID of the quest to check.</param>
     /// <returns>True if the quest is completed, otherwise false.</returns>
-    public bool isCompletedBefore(int QuestID, bool log = true)
+    public bool isCompletedBefore(int QuestID = 0, bool log = true)
     {
+        if (QuestID <= 0)
+            return false;
+
         Quest? quest = InitializeWithRetries(() => EnsureLoad(QuestID));
         if (quest == null)
         {
