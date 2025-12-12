df43266ec67e7cf5608ec122727a4a91e128dada
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-11 21:26:40 -0500

    Update Core13LoC.cs

--- Story/LordsofChaos/Core13LoC.cs
+++ Story/LordsofChaos/Core13LoC.cs
@@ -298,7 +298,12 @@ public class Core13LoC
         Story.KillQuest(340, "dwarfhold", "Albino Bat");
 
         //Rock me Amadeus
-        Story.KillQuest(341, "dwarfhold", "Amadeus");
+        if (!Story.QuestProgression(341))
+        {
+            Core.EnsureAccept(341);
+            Core.HuntMonster("dwarfhold", "Amadeus", "Key Mold");
+            Core.EnsureComplete(341);
+        }
 
         // //Upper City Gates
         // if (!Story.QuestProgression(343))
