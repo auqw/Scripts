4c56e270b04f4357a9d9c4ef1546242afc40e431
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2025-12-13 15:52:26 +0100

    Refactor CoreNation.cs: Clean up whitespace and comment out unused method

--- Nation/CoreNation.cs
+++ Nation/CoreNation.cs
@@ -538,7 +538,6 @@ public class CoreNation
             Core.EnsureComplete(7551, Item.ID);
             SellVoucherOfNulgath(sellMemVoucher, item);
 
-
             Core.FarmingLogger(Item.Name, quant);
         }
     }
@@ -1491,7 +1490,7 @@ public class CoreNation
             goto Retry7551;
         }
 
-    Retry2859:
+        Retry2859:
         Quest? Assistant = Core.InitializeWithRetries(() => Bot.Quests.EnsureLoad(2859));
         if (Assistant == null)
         {
@@ -1834,7 +1833,7 @@ public class CoreNation
             // Do Swindles Return Policy if enabled
             DoSwindlesReturnArea(returnPolicyDuringSupplies, ReturnItem);
 
-        Retry:
+            Retry:
             //reduce spam
             Quest? quest = Core.InitializeWithRetries(() => Bot.Quests.EnsureLoad(7551));
             if (quest != null)
@@ -2339,7 +2338,7 @@ public class CoreNation
     public void FarmTaintedGem(int quant = 1000)
     {
         if (Core.CheckInventory("Tainted Gem", quant))
-            return;            
+            return;
 
         Core.AddDrop("Tainted Gem");
         FarmContractExchage("Tainted Gem", quant);
@@ -3319,10 +3318,9 @@ public class CoreNation
         Core.CancelRegisteredQuests();
     }
 
-
-    private int GetQuestRewardMaxStack(string itemName, int QuestID = 555) =>
-        Core.InitializeWithRetries(() => Bot.Quests.EnsureLoad(QuestID), 20)
-            .Rewards.FirstOrDefault(x => x != null && x.Name == itemName)?.MaxStack ?? 0;
+    // private int GetQuestRewardMaxStack(string itemName, int QuestID = 555) =>
+    //     Core.InitializeWithRetries(() => Bot.Quests.EnsureLoad(QuestID), 20)
+    //         .Rewards.FirstOrDefault(x => x != null && x.Name == itemName)?.MaxStack ?? 0;
 }
 
 public enum ChooseReward
