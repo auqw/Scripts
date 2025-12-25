44b1ce5198c12d658bbff83374a695fd45bb3fe8
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-25 06:37:26 -0500

    Create TagAllAccounts.cs

---
+++ Army/OneClient/TagAllAccounts.cs
@@ -0,0 +1,48 @@
+/*
+name: Tag All
+description: use the AutoTag to tag all accounds in the manager.
+tags: tags, autotag, auto tag
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreDailies.cs
+//cs_include Scripts/CoreStory.cs
+//cs_include Scripts/CoreAdvanced.cs
+//cs_include Scripts/CoreFarms.cs
+//cs_include Scripts/Army/CoreArmyLite.cs
+using Skua.Core.Interfaces;
+
+public class TagAll
+{
+    private IScriptInterface Bot => IScriptInterface.Instance;
+    private CoreBots Core => CoreBots.Instance;
+    private static CoreArmyLite Army
+    {
+        get => _Army ??= new CoreArmyLite();
+        set => _Army = value;
+    }
+    private static CoreArmyLite _Army;
+
+    public void ScriptMain(IScriptInterface Bot)
+    {
+        Core.SetOptions();
+
+        DoTheThing();
+
+        Core.SetOptions(false);
+    }
+
+    public void DoTheThing()
+    {
+        List<ManagedAccount> accounts = Bot.Accounts.GetAllAccounts();
+        int AccCount = accounts.Count;
+        int i = 1;
+
+        while (!Bot.ShouldExit && Army.doForAll())
+        {
+            Core.Logger($"Adding Tags to {Bot.Player.Username} [Account {i}/{AccCount}], please be patient", "Tag All");
+            Core.AutoAddTags();
+            Core.Logger($"Tags for {Bot.Player.Username}, finished!", "Tag All");
+            i++;
+        }
+    }
+}
