25c6fff31486424ef689b8ff73f6ad20e2e2c3b4
Tato <47118168+wtffidy@users.noreply.github.com>
2025-12-26 10:56:01 -0500

    .

--- Good/GearOfAwe/ArmorOfAwe.cs
+++ Good/GearOfAwe/ArmorOfAwe.cs
@@ -65,11 +65,8 @@ public class ArmorOfAwe
                 Core.EquipClass(ClassType.Solo);
 
                 Core.RegisterQuests(4162);
-                while (!Bot.ShouldExit && !Core.CheckInventory("Pauldron Fragment", 15))
-                {
-                    Core.HuntMonster("gravestrike", "Ultra Akriloth", "Pauldron Shard", 15, false);
-                    Bot.Wait.ForPickup("Pauldron Fragment");
-                }
+                Core.HuntMonster("gravestrike", "Ultra Akriloth", "Pauldron Fragment", 15, false);
+                Bot.Wait.ForPickup("Pauldron Fragment");
                 Core.CancelRegisteredQuests();
 
                 Core.BuyItem("museum", 1129, "Pauldron Relic");
