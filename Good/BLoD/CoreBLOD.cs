e43985f54227abbf4d0b9ef0ce4d087fdc7a6d78
Tato <47118168+wtffidy@users.noreply.github.com>
2026-01-01 12:41:49 -0500

    Update CoreBLOD.cs

--- Good/BLoD/CoreBLOD.cs
+++ Good/BLoD/CoreBLOD.cs
@@ -613,16 +613,14 @@ public class CoreBLOD
         string weaponName = weapon + " of Destiny";
         if (Core.CheckInventory(weaponName))
             return;
-
         List<ItemBase> weaponReqs = (LightMergeShopItems ??= Core.GetShopItems("necropolis", 422))
             .First(item => item.Name == weaponName)
             .Requirements;
         ItemBase metal = weaponReqs.First(req => req.Name.EndsWith("of Destiny"));
         UpgradeMetal(
             (MineCraftingMetalsEnum)
-                Enum.Parse(typeof(MineCraftingMetalsEnum), metal.Name.Split(' ')[1])
+                Enum.Parse(typeof(MineCraftingMetalsEnum), metal.Name.Split(' ')[0])  // Changed from [1] to [0]
         );
-
         GetMergeRequirements(weaponReqs, metal.ID);
         LightMerge(weaponName);
     }
