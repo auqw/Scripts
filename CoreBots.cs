28fbe4553d65aba7f92b2c3ac06ce44ecb76b8e7
SharpTheNightmare <koen.t04@gmail.com>
2026-01-05 03:27:03 -0600

    test for auto tag

--- CoreBots.cs
+++ CoreBots.cs
@@ -6517,7 +6517,7 @@ public class CoreBots
         }
         if (TagsToAdd.Count > 0)
         {
-            acc.Tags.AddRange(TagsToAdd);  // Pass the List<string> directly
+            Bot.Accounts.AddTags(Bot.Player.Username, TagsToAdd.ToArray());
             string tagsToAddString = string.Join(", ", TagsToAdd);
             Logger(tagsToAddString);
         }
