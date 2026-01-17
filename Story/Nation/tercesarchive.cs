949942df08c4b252c7c610eabc4a2354fd116f5c
Tato <47118168+wtffidy@users.noreply.github.com>
2026-01-17 00:21:40 -0500

    new story fuck off

---
+++ Story/Nation/tercesarchive.cs
@@ -0,0 +1,116 @@
+/*
+name: tercesarchive
+description: This will finish the tercesarchive quest.
+tags: story, quest, tercesarchive, nation
+*/
+//cs_include Scripts/CoreBots.cs
+//cs_include Scripts/CoreStory.cs
+using Skua.Core.Interfaces;
+
+public class tercesarchive
+{
+    public IScriptInterface Bot => IScriptInterface.Instance;
+    public CoreBots Core => CoreBots.Instance;
+    private static CoreStory Story
+    {
+        get => _Story ??= new CoreStory();
+        set => _Story = value;
+    }
+    private static CoreStory _Story;
+
+    public void ScriptMain(IScriptInterface bot)
+    {
+        Core.SetOptions();
+
+        StoryLine();
+
+        Core.SetOptions(false);
+    }
+
+    public void StoryLine()
+    {
+        if (Core.isCompletedBefore(10556))
+            return;
+
+        Story.PreLoad(this);
+
+        #region Useable Monsters
+        string[] UseableMonsters = new[]
+        {
+            "Oblivion Magus", // UseableMonsters[0],
+            "Fiendish Devourer", // UseableMonsters[1],
+            "Nightmare Fiend", // UseableMonsters[2],
+            "Wasted Clone", // UseableMonsters[3],
+            "Fiend of Voracity", // UseableMonsters[4]
+        };
+        #endregion Useable Monsters
+
+        // 10549 | Oblivious Oblivion
+        if (!Story.QuestProgression(10549))
+        {
+            Core.HuntMonsterQuest(10549,
+                ("tercesarchive", UseableMonsters[0], ClassType.Farm));
+        }
+
+
+        // 10550 | Tongue and Cheek
+        if (!Story.QuestProgression(10550))
+        {
+            Core.HuntMonsterQuest(10550,
+                ("tercesarchive", UseableMonsters[1], ClassType.Farm));
+        }
+
+
+        // 10551 | O.T.
+        if (!Story.QuestProgression(10551))
+        {
+            Core.HuntMonsterQuest(10551,
+                ("tercesarchive", UseableMonsters[0], ClassType.Farm),
+                ("tercesarchive", UseableMonsters[1], ClassType.Farm));
+        }
+
+
+        // 10552 | The Fiend is in the Details
+        if (!Story.QuestProgression(10552))
+        {
+            Story.MapItemQuest(10552, "tercesarchive", 15393);
+            Story.MapItemQuest(10552, "tercesarchive", 15394);
+        }
+
+
+        // 10553 | Soulectomy
+        if (!Story.QuestProgression(10553))
+        {
+            Core.HuntMonsterQuest(10553,
+                ("tercesarchive", UseableMonsters[3], ClassType.Farm));
+        }
+
+
+        // 10554 | A Nation Night Terror
+        if (!Story.QuestProgression(10554))
+        {
+            Core.HuntMonsterQuest(10554,
+                ("tercesarchive", UseableMonsters[2], ClassType.Farm));
+        }
+
+
+        // 10555 | Point of No Return
+        if (!Story.QuestProgression(10555))
+        {
+            Core.HuntMonsterQuest(10555,
+                ("tercesarchive", UseableMonsters[3], ClassType.Farm),
+                ("tercesarchive", UseableMonsters[2], ClassType.Farm));
+        }
+
+
+        // 10556 | All Appetite
+        if (!Story.QuestProgression(10556))
+        {
+            Core.HuntMonsterQuest(10556,
+                ("tercesarchive", UseableMonsters[4], ClassType.Solo));
+        }
+    }
+
+
+
+}
