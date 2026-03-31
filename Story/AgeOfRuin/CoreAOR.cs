8f5635373c80c6f5cd22f9bfc36d4f6e7a160f65
Tato <47118168+wtffidy@users.noreply.github.com>
2026-03-31 02:44:34 -0400

    Update CoreAOR.cs

--- Story/AgeOfRuin/CoreAOR.cs
+++ Story/AgeOfRuin/CoreAOR.cs
@@ -736,7 +736,7 @@ public class CoreAOR
 
         // Last Snowfall (9810)
         Story.MapItemQuest(9810, "liatarahill", 13368);
-        Story.KillQuest(9810, "liatarahill", new[] { "Garde Wraith", "Undead Garde" });
+        Story.KillQuest(9810, "liatarahill", new[] { "Undead Garde", "Garde Wraith" });
 
         // Spring Melt (9811)
         Story.MapItemQuest(9811, "liatarahill", 13366);
