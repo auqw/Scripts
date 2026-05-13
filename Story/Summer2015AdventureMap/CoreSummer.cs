ea62551faa4837a6c7fe1d7eaa223edd8c32800f
Tato <47118168+wtffidy@users.noreply.github.com>
2026-05-13 11:04:56 -0400

    Update CoreSummer.cs

--- Story/Summer2015AdventureMap/CoreSummer.cs
+++ Story/Summer2015AdventureMap/CoreSummer.cs
@@ -84,13 +84,12 @@ public class CoreSummer
         Story.KillQuest(4268, "dreadspace", "Cyber Horg");
 
         //Fix the Holodeck (4269)
-
         if (!Story.QuestProgression(4269))
         {
             Core.EnsureAccept(4269);
-            Core.HuntMonster("dreadspace", "Undead Space Marine", "Holodeck Force Field", 7);
-            Core.HuntMonster("dreadspace", "Undead Space Warrior", "Holodeck EbilCorp AI");
-            Core.HuntMonster("dreadspace", "Undead Space Warrior", "Holodeck Projector", 7);
+            Core.HuntMonster("dreadspace", "Undead Space Warrior", "Holodeck Force Field", 7);
+            Core.HuntMonster("dreadspace", "Undead Star Ranger", "Holodeck EbilCorp AI");
+            Core.HuntMonster("dreadspace", "Undead Space Marine", "Holodeck Projector", 7);
             Core.HuntMonster("dreadspace", "Cyber Horg", "Holodeck Power Source");
             Core.EnsureComplete(4269);
         }
