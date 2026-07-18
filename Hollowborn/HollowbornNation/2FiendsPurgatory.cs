8331fb324b5f115ad75125692516de1bb33c4801
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2026-07-18 13:15:03 +0200

    Fix reward retrieval in Fiend's Purgatory quest and clean up code

--- Hollowborn/HollowbornNation/2FiendsPurgatory.cs
+++ Hollowborn/HollowbornNation/2FiendsPurgatory.cs
@@ -132,7 +132,7 @@ public class FiendsPurgatory
 
         if (QuestOnly)
         {
-            string[] rewards = [.. Core.InitializeWithRetries(() => Core.QuestRewards(QuestID).Where(x => x != "Void Soul"))];
+            string[] rewards = [.. (Core.InitializeWithRetries(() => Core.QuestRewards(QuestID)) ?? []).Where(x => x != "Void Soul")];
             if (rewards.Length == 0)
             {
                 Core.Logger($"No rewards found for quest {QuestID} *or* Quest !Failed! to load, skipping.");
@@ -144,7 +144,7 @@ public class FiendsPurgatory
             // Fake complete `Self-Destructive Spell` to see boss
             Bot.Quests.UpdateQuest(10030);
             Core.HuntMonster("deleuzetundra", "Blighted Zubami", "Blighted Zubami Badge", isTemp: false);
-           
+
             Core.HuntMonster("voidsalek", "Salek Sprayer", "Spoil of Salek", isTemp: false);
             Core.HuntMonster("voidnerfkitten", "Sarah the Nerfkitten", "Sarah's Souvenir", isTemp: false);
 
