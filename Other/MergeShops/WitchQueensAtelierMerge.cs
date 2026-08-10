16aa4cc6b6a52ac2c24acd702812a380eac8cb86
seolyam <leeyam.dev@gmail.com>
2026-08-10 19:32:44 +0800

    Update WitchQueensAtelierMerge: add ArmyMode and jump to r2

--- Other/MergeShops/WitchQueensAtelierMerge.cs
+++ Other/MergeShops/WitchQueensAtelierMerge.cs
@@ -24,8 +24,8 @@ public class WitchQueensAtelierMerge
 
     public bool DontPreconfigure = true;
     public List<IOption> Generic = sAdv.MergeOptions;
-    public string[] MultiOptions = { "Generic", "Select" };
-    public string OptionsStorage = sAdv.OptionsStorage;
+    public string[] MultiOptions = { "Generic", "Select", "ArmySetup" };
+    public string OptionsStorage = "WitchQueensAtelierMerge";
     // [Can Change] This should only be changed by the author.
     //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
     private bool dontStopMissingIng = false;
@@ -34,6 +34,8 @@ public class WitchQueensAtelierMerge
     {
         Core.BankingBlackList.AddRange(new[] { "Gold Voucher 100k", "Witch Princess' Hat", "Starlight Chaser's Morph", "Starlight Chaser's Visage", "Crystalized Darkovian Tear", "Witch Princess' Astrolabe", "Galilera" });
         Core.SetOptions();
+
+        Core.Logger($"[Debug] armyMode is currently: {Bot.Config.Get<bool>("ArmySetup", "armyMode")}");
 
         BuyAllMerge();
         Core.SetOptions(false);
@@ -65,12 +67,35 @@ public class WitchQueensAtelierMerge
 
                 case "Witch Princess' Hat":
                     Core.FarmingLogger(req.Name, quant);
-                    Core.EquipClass(ClassType.Solo);
+                    if (Bot.Config.Get<bool>("ArmySetup", "armyMode"))
+                        EquipArmyClass();
+                    else
+                        Core.EquipClass(ClassType.Solo);
+                    
                     Core.RegisterQuests(10824);
-                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    if (Bot.Config.Get<bool>("ArmySetup", "armyMode"))
                     {
-                        Core.KillMonster("birgittaspire", "r2", "Left", "Witch Queen Talia", req.Name, quant, isTemp: false, log: false);
-                        Bot.Wait.ForPickup(req.Name);
+                        Core.Logger("Farming with Army");
+                        Core.Join("birgittaspire", "r2", "Left");
+                        Core.Sleep();
+                        while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                        {
+                            if (Bot.Player.Cell != "r2")
+                            {
+                                Core.Jump("r2", "Left");
+                                Bot.Sleep(1000);
+                            }
+                            Bot.Combat.Attack("Witch Queen Talia");
+                            Bot.Sleep(Core.ActionDelay);
+                        }
+                    }
+                    else
+                    {
+                        while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                        {
+                            Core.KillMonster("birgittaspire", "r2", "Left", "Witch Queen Talia", req.Name, quant, isTemp: false, log: false);
+                            Bot.Wait.ForPickup(req.Name);
+                        }
                     }
                     Core.CancelRegisteredQuests();
                     break;
@@ -78,11 +103,34 @@ public class WitchQueensAtelierMerge
                 case "Crystalized Darkovian Tear":
                 case "Witch Princess' Astrolabe":
                     Core.FarmingLogger(req.Name, quant);
-                    Core.EquipClass(ClassType.Solo);
-                    while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                    if (Bot.Config.Get<bool>("ArmySetup", "armyMode"))
+                        EquipArmyClass();
+                    else
+                        Core.EquipClass(ClassType.Solo);
+                    
+                    if (Bot.Config.Get<bool>("ArmySetup", "armyMode"))
                     {
-                        Core.KillMonster("birgittaspire", "r2", "Left", "Witch Queen Talia", req.Name, quant, isTemp: false, log: false);
-                        Bot.Wait.ForPickup(req.Name);
+                        Core.Logger("Farming with Army");
+                        Core.Join("birgittaspire", "r2", "Left");
+                        Core.Sleep();
+                        while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                        {
+                            if (Bot.Player.Cell != "r2")
+                            {
+                                Core.Jump("r2", "Left");
+                                Bot.Sleep(1000);
+                            }
+                            Bot.Combat.Attack("Witch Queen Talia");
+                            Bot.Sleep(Core.ActionDelay);
+                        }
+                    }
+                    else
+                    {
+                        while (!Bot.ShouldExit && !Core.CheckInventory(req.Name, quant))
+                        {
+                            Core.KillMonster("birgittaspire", "r2", "Left", "Witch Queen Talia", req.Name, quant, isTemp: false, log: false);
+                            Bot.Wait.ForPickup(req.Name);
+                        }
                     }
                     break;
 
@@ -104,4 +152,56 @@ public class WitchQueensAtelierMerge
         new Option<bool>("Starlight Witch Hat", "Starlight Witch Hat", "Mode: [select] only\nShould the bot buy \"Starlight Witch Hat\" ?", false),
         new Option<bool>("Visionary Astrolabe", "Visionary Astrolabe", "Mode: [select] only\nShould the bot buy \"Visionary Astrolabe\" ?", false),
     };
+
+    public List<IOption> ArmySetup = new()
+    {
+        new Option<bool>("armyMode", "Enable Army Mode", "Enable army mode for farming the boss", false),
+        new Option<string>("Account1", "Account 1", "Format: Username,ClassName", ""),
+        new Option<string>("Account2", "Account 2", "Format: Username,ClassName", ""),
+        new Option<string>("Account3", "Account 3", "Format: Username,ClassName", ""),
+        new Option<string>("Account4", "Account 4", "Format: Username,ClassName", ""),
+        new Option<string>("Account5", "Account 5", "Format: Username,ClassName", ""),
+        new Option<string>("Account6", "Account 6", "Format: Username,ClassName", ""),
+        new Option<string>("Account7", "Account 7", "Format: Username,ClassName", "")
+    };
+
+    private void EquipArmyClass()
+    {
+        string username = Core.Username().Trim().ToLower();
+        Core.Logger($"[ArmyClass] Finding class for '{username}'");
+
+        for (int i = 1; i <= 7; i++)
+        {
+            string accOpt = Bot.Config.Get<string>("ArmySetup", $"Account{i}") ?? "";
+            if (string.IsNullOrWhiteSpace(accOpt)) continue;
+
+            string[] parts = accOpt.Split(',');
+            if (parts.Length > 1)
+            {
+                string targetUser = parts[0].Trim().ToLower();
+                string className = parts[1].Trim();
+
+                Core.Logger($"[ArmyClass] Account{i}: Target='{targetUser}' Class='{className}'");
+
+                if (targetUser == username)
+                {
+                    Core.Logger($"[ArmyClass] Match found! Equipping '{className}'");
+                    Core.JumpWait();
+                    if (Core.CheckInventory(className, toInv: false))
+                        Core.Unbank(className);
+                    Core.Equip(className);
+                    Adv.SmartEnhance(className);
+                    Bot.Skills.StartAdvanced(className, true);
+                    return;
+                }
+            }
+            else
+            {
+                Core.Logger($"[ArmyClass] Account{i} is invalid ('{accOpt}'). Format must be Username,ClassName");
+            }
+        }
+        
+        Core.Logger($"[ArmyClass] No match for '{username}'. Falling back to default Solo class.");
+        Core.EquipClass(ClassType.Solo);
+    }
 }
