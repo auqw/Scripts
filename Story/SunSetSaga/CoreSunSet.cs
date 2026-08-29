fa9129349ecf21d4a36c0c5c0d4031a85b36bce9
Tato <47118168+wtffidy@users.noreply.github.com>
2026-08-29 08:47:15 -0400

    Update CoreSunSet.cs

--- Story/SunSetSaga/CoreSunSet.cs
+++ Story/SunSetSaga/CoreSunSet.cs
@@ -61,39 +61,59 @@ public class CoreSunSet
 
         // 10836 | Within a Raw Wound
         Story.MapItemQuest(10836, "templeofdoom", [16097, 16101]);
+        Core.HuntMonsterQuest(
+            10836,
+            ("templeofdoom", UseableMonsters[2], ClassType.Farm) // Erased Text x12
+        );
 
         // 10837 | Trust is the Cure
-        Core.EquipClass(ClassType.Farm);
         Story.MapItemQuest(10837, "templeofdoom", [16102, 16114]);
-        Story.KillQuest(10837, "templeofdoom", UseableMonsters[1]); // Drained Leech x15
+        Core.HuntMonsterQuest(
+            10837,
+            ("templeofdoom", UseableMonsters[1], ClassType.Farm) // Drained Leech x15
+        );
 
         // 10838 | Inferiority Builds Character
         Story.MapItemQuest(10838, "templeofdoom", [16115, 16116, 16117]);
-        Story.KillQuest(10838, "templeofdoom", UseableMonsters[4]); // Corrupted Gold x6
+        Core.HuntMonsterQuest(
+            10838,
+            ("templeofdoom", UseableMonsters[4], ClassType.Farm) // Corrupted Gold x6
+        );
 
         // 10839 | Mission Briefing
         Story.MapItemQuest(10839, "templeofdoom", [16118, 16119]);
 
         // 10840 | The Light's Reward
         Story.MapItemQuest(10840, "templeofdoom", 16120);
-        Story.KillQuest(10840, "templeofdoom", UseableMonsters[4]); // Erased Paladin x15
+        Core.HuntMonsterQuest(
+            10840,
+            ("templeofdoom", UseableMonsters[4], ClassType.Farm) // Erased Paladin x15
+        );
 
         // 10841 | Wordless Cipher
         Story.MapItemQuest(10841, "templeofdoom", 16121);
 
         // 10842 | Attention! Salute!
         Story.MapItemQuest(10842, "templeofdoom", [16122, 16123, 16124]);
-        Story.KillQuest(10842, "templeofdoom", UseableMonsters[2]); // Deletion Denied x18
+        Core.HuntMonsterQuest(
+            10842,
+            ("templeofdoom", UseableMonsters[2], ClassType.Farm) // Deletion Denied x18
+        );
 
         // 10843 | All Roads Lead To…
         Story.MapItemQuest(10843, "templeofdoom", 16125, 3);
 
         // 10844 | Law and Order
-        Story.KillQuest(10844, "templeofdoom", UseableMonsters[0]); // Aegis' Stone Veil x6
+        Core.HuntMonsterQuest(
+            10844,
+            ("templeofdoom", UseableMonsters[0], ClassType.Farm) // Aegis' Stone Veil x6
+        );
 
         // 10845 | There's an Order to These Things
-        Core.EquipClass(ClassType.Solo);
-        Story.KillQuest(10845, "templeofdoom", UseableMonsters[3]); // Sentinel of Order Defeated x1
+        Core.HuntMonsterQuest(
+            10845,
+            ("templeofdoom", UseableMonsters[3], ClassType.Solo) // Sentinel of Order Defeated x1
+        );
     }
 
 }
