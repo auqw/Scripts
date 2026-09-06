c533a4a56a14e81ca680e68b2b85b28e0dced5ac
Tato <47118168+wtffidy@users.noreply.github.com>
2026-09-06 11:20:42 -0400

    Update PovoHeroicoMerge.cs

--- Other/MergeShops/PovoHeroicoMerge.cs
+++ Other/MergeShops/PovoHeroicoMerge.cs
@@ -1,7 +1,7 @@
 /*
 name: Povo Heroico Merge
-description: This bot will farm the items belonging to the selected mode for the Povo Heroico Merge [2620] in /povoheroico
-tags: povo, heroico, merge, povoheroico, brado, encantado, capuz, face, encantada, símbolo, da, ordem, do, progresso, brabo, brabos, encantados
+description: Farms the Povo Heroico Merge [2620] in /povoheroico.
+tags: povoheroico, merge, povo, heroico, merge
 */
 //cs_include Scripts/CoreBots.cs
 //cs_include Scripts/CoreFarms.cs
@@ -10,161 +10,104 @@ using Skua.Core.Interfaces;
 using Skua.Core.Models.Items;
 using Skua.Core.Options;
 
-public class PovoHeroicoMerge
+public class PovoHeroico
 {
     private IScriptInterface Bot => IScriptInterface.Instance;
     private CoreBots Core => CoreBots.Instance;
-    private static CoreFarms Farm
-    {
-        get => _Farm ??= new CoreFarms();
-        set => _Farm = value;
-    }
+    private static CoreFarms Farm { get => _Farm ??= new CoreFarms(); set => _Farm = value; }
     private static CoreFarms _Farm;
-    private static CoreAdvanced Adv
-    {
-        get => _Adv ??= new CoreAdvanced();
-        set => _Adv = value;
-    }
+    private static CoreAdvanced Adv { get => _Adv ??= new CoreAdvanced(); set => _Adv = value; }
     private static CoreAdvanced _Adv;
-    private static CoreAdvanced sAdv
-    {
-        get => _sAdv ??= new CoreAdvanced();
-        set => _sAdv = value;
-    }
+    private static CoreAdvanced sAdv { get => _sAdv ??= new CoreAdvanced(); set => _sAdv = value; }
     private static CoreAdvanced _sAdv;
 
     public bool DontPreconfigure = true;
     public List<IOption> Generic = sAdv.MergeOptions;
     public string[] MultiOptions = { "Generic", "Select" };
     public string OptionsStorage = sAdv.OptionsStorage;
-
-    // [Can Change] This should only be changed by the author.
-    //              If true, it will not stop the script if the default case triggers and the user chose to only get mats
     private bool dontStopMissingIng = false;
 
     public void ScriptMain(IScriptInterface Bot)
     {
-        Core.BankingBlackList.AddRange(
-            new[]
-            {
-                "Moeda Real",
-                "Brado Retumbante",
-                "Capuz Retumbante",
-                "Face Retumbante",
-                "Símbolo da Ordem",
-                "Símbolo do Progresso",
-                "Brabo Retumbante",
-                "Brabos Retumbantes",
-            }
-        );
+        Core.BankingBlackList.AddRange([
+            "Cabelo do Meio-Dia",
+            "Corrente da Liberdade",
+            "Corrente do Sol",
+            "Correntes da Liberdade",
+            "Correntes do Sol",
+            "Corte do Meio-Dia",
+            "Face do Meio-Dia",
+            "Maxado de Assís",
+            "Moeda Real do Sol",
+            "Nascer do Sol",
+            "Olhar do Meio-Dia",
+            "Raios de Sol",
+            "Raios Fúlgidos",
+            "Runa do Sol da Liberdade",
+            "Símbolo do Sol da Liberdade",
+            "Sol da Liberdade",
+        ]);
         Core.SetOptions();
-
         BuyAllMerge();
         Core.SetOptions(false);
     }
 
     public void BuyAllMerge(string? buyOnlyThis = null, mergeOptionsEnum? buyMode = null)
     {
-        //Only edit the map and shopID here
         Adv.StartBuyAllMerge("povoheroico", 2620, findIngredients, buyOnlyThis, buyMode: buyMode);
 
-        #region Dont edit this part
         void findIngredients()
         {
             ItemBase req = Adv.externalItem;
             int quant = Adv.externalQuant;
-            int currentQuant = req.Temp
-                ? Bot.TempInv.GetQuantity(req.Name)
-                : Bot.Inventory.GetQuantity(req.Name);
             if (req == null)
-            {
-                Core.Logger("req is NULL");
                 return;
-            }
 
             switch (req.Name)
             {
+                case "Cabelo do Meio-Dia":
+                case "Corrente da Liberdade":
+                case "Corrente do Sol":
+                case "Correntes da Liberdade":
+                case "Correntes do Sol":
+                case "Corte do Meio-Dia":
+                case "Face do Meio-Dia":
+                case "Maxado de Assís":
+                case "Moeda Real do Sol":
+                case "Nascer do Sol":
+                case "Olhar do Meio-Dia":
+                case "Raios de Sol":
+                case "Raios Fúlgidos":
+                case "Runa do Sol da Liberdade":
+                case "Símbolo do Sol da Liberdade":
+                case "Sol da Liberdade":
+                    Core.EquipClass(ClassType.Solo);
+                    Core.HuntMonster("povoheroico", "Sol Da Liberdade", req.Name, quant, req.Temp);
+                    break;
                 default:
                     bool shouldStop = !Adv.matsOnly || !dontStopMissingIng;
-                    Core.Logger(
-                        $"The bot hasn't been taught how to get {req.Name}."
-                            + (shouldStop ? " Please report the issue." : " Skipping"),
-                        messageBox: shouldStop,
-                        stopBot: shouldStop
-                    );
-                    break;
-        #endregion
-
-                // All of these drop from the monster "Brado Retumbante" in povoheroico (cell: r2, pad: Bottom)
-                case "Moeda Real":
-                case "Brabo Retumbante":
-                case "Brabos Retumbantes":
-                case "Brado Retumbante":
-                case "Capuz Retumbante":
-                case "Face Retumbante":
-                case "Símbolo da Ordem":
-                case "Símbolo do Progresso":
-                    Core.FarmingLogger(req.Name, quant);
-                    Core.EquipClass(ClassType.Solo);
-
-                    Core.KillMonster(
-                        "povoheroico",
-                        "r2",
-                        "Bottom",
-                        "Brado Retumbante",
-                        req.Name,
-                        quant,
-                        req.Temp
-                    );
-                    Bot.Wait.ForPickup(req.Name);
+                    Core.Logger($"The bot hasn't been taught how to get {req.Name}.", messageBox: shouldStop, stopBot: shouldStop);
                     break;
             }
         }
     }
 
-    public List<IOption> Select = new()
-    {
-        new Option<bool>(
-            "95277",
-            "Brado Encantado",
-            "Mode: [select] only\nShould the bot buy \"Brado Encantado\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95278",
-            "Capuz Encantado",
-            "Mode: [select] only\nShould the bot buy \"Capuz Encantado\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95279",
-            "Face Encantada",
-            "Mode: [select] only\nShould the bot buy \"Face Encantada\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95280",
-            "Símbolo da Ordem Encantado",
-            "Mode: [select] only\nShould the bot buy \"Símbolo da Ordem Encantado\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95281",
-            "Símbolo do Progresso Encantado",
-            "Mode: [select] only\nShould the bot buy \"Símbolo do Progresso Encantado\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95282",
-            "Brabo Encantado",
-            "Mode: [select] only\nShould the bot buy \"Brabo Encantado\" ?",
-            false
-        ),
-        new Option<bool>(
-            "95283",
-            "Brabos Encantados",
-            "Mode: [select] only\nShould the bot buy \"Brabos Encantados\" ?",
-            false
-        ),
-    };
+    public List<IOption> Select =
+    [
+        new Option<bool>("103081", "Sol Encantado da Liberdade", "Mode: [select] only\nShould the bot buy \"Sol Encantado da Liberdade\" ?", false),
+        new Option<bool>("103089", "Maxado de Assís Encantado", "Mode: [select] only\nShould the bot buy \"Maxado de Assís Encantado\" ?", false),
+        new Option<bool>("103086", "Raios de Sol Encantados", "Mode: [select] only\nShould the bot buy \"Raios de Sol Encantados\" ?", false),
+        new Option<bool>("103087", "Símbolo Encantado do Sol da Liberdade", "Mode: [select] only\nShould the bot buy \"Símbolo Encantado do Sol da Liberdade\" ?", false),
+        new Option<bool>("103093", "Correntes Encantadas da Liberdade", "Mode: [select] only\nShould the bot buy \"Correntes Encantadas da Liberdade\" ?", false),
+        new Option<bool>("103095", "Correntes Encantadas do Sol", "Mode: [select] only\nShould the bot buy \"Correntes Encantadas do Sol\" ?", false),
+        new Option<bool>("103090", "Raios Fúlgidos Encatados", "Mode: [select] only\nShould the bot buy \"Raios Fúlgidos Encatados\" ?", false),
+        new Option<bool>("103091", "Nascer do Sol Encantado", "Mode: [select] only\nShould the bot buy \"Nascer do Sol Encantado\" ?", false),
+        new Option<bool>("103082", "Olhar Encantado do Meio-Dia", "Mode: [select] only\nShould the bot buy \"Olhar Encantado do Meio-Dia\" ?", false),
+        new Option<bool>("103083", "Face Encantada do Meio-Dia", "Mode: [select] only\nShould the bot buy \"Face Encantada do Meio-Dia\" ?", false),
+        new Option<bool>("103084", "Cabelo Encantado do Meio-Dia", "Mode: [select] only\nShould the bot buy \"Cabelo Encantado do Meio-Dia\" ?", false),
+        new Option<bool>("103085", "Corte Encantado do Meio-Dia", "Mode: [select] only\nShould the bot buy \"Corte Encantado do Meio-Dia\" ?", false),
+        new Option<bool>("103092", "Corrente Encantada da Liberdade", "Mode: [select] only\nShould the bot buy \"Corrente Encantada da Liberdade\" ?", false),
+        new Option<bool>("103094", "Corrente Encantada do Sol", "Mode: [select] only\nShould the bot buy \"Corrente Encantada do Sol\" ?", false),
+        new Option<bool>("103088", "Runa Encantada do Sol da Liberdade", "Mode: [select] only\nShould the bot buy \"Runa Encantada do Sol da Liberdade\" ?", false),
+    ];
 }
